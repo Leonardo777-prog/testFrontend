26bb58626dd9df17e76daa604271cd86a57a2a5a
Leonardo Cano <leomaucano111@gmail.com>
2021-05-21 15:47:58 -0300

    estructuración de las carpetas

---
+++ r3-modal/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Card } from "./components/Card";
+
+const App = () => {
+  return (
+    <>
+      <h1>Ejercicio 2</h1>
+      <Card />
+    </>
+  );
+};
+
+export default App;
