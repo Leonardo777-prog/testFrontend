9a99a59efa4915e75a469e82baa84b2336c380cf
Leonardo Cano <leomaucano111@gmail.com>
2021-05-21 16:25:40 -0300

    posisionamiento del boton de play en la card

--- r3-modal/src/components/Card.jsx
+++ r3-modal/src/components/Card.jsx
@@ -3,8 +3,27 @@ import React from "react";
 export const Card = ({ urlImage, title, texto }) => {
   return (
     <div className="card">
-      <div>
+      <div className="contenedor-imagen">
         <img src={urlImage} alt={title} loading="lazy" />
+        <div className="contenedor-icono">
+          <div className="icono-play">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-player-play"
+              width="76"
+              height="76"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#ffffff"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M7 4v16l13 -8z" />
+            </svg>
+          </div>
+        </div>
       </div>
       <div>
         <h2>{title}</h2>
